478173f5e502de54e65cfd28145c2395eb9cf9cf
anaclarabck <ackyotoku@gmail.com>
2021-07-21 17:06:22 -0300

    Conclui exercicio um

--- src/Cars.jsx
+++ src/Cars.jsx
@@ -6,51 +6,56 @@ import { connect } from 'react-redux';
 import carBlue from './images/carBlue.jpeg';
 import carRed from './images/carRed.jpeg';
 import carYellow from './images/carYellow.jpeg';
+import MyContext from './MyContext';
 import { moveCar } from './redux/actionCreators';
 
 function Cars({ redCar, blueCar, yellowCar, moveCar }) {
   return (
-    <div>
-      <div>
-        <img
-          className={redCar ? 'car-right' : 'car-left'}
-          src={carRed}
-          alt="red car"
-        />
-        <button
-          onClick={() => moveCar('red', !redCar)}
-          type="button"
-        >
-          Move
-        </button>
-      </div>
-      <div>
-        <img
-          className={blueCar ? 'car-right' : 'car-left'}
-          src={carBlue}
-          alt="blue car"
-        />
-        <button
-          onClick={() => moveCar('blue', !blueCar)}
-          type="button"
-        >
-          Move
-        </button>
-      </div>
-      <div>
-        <img
-          className={yellowCar ? 'car-right' : 'car-left'}
-          src={carYellow}
-          alt="yellow car"
-        />
-        <button
-          onClick={() => moveCar('yellow', !yellowCar)}
-          type="button"
-        >
-          Move
-        </button>
-      </div>
-    </div>
+    <MyContext.Consumer>
+      { value => (
+        <div>
+          <div>
+            <img
+              className={value.redCar ? 'car-right' : 'car-left'}
+              src={carRed}
+              alt="red car"
+            />
+            <button
+              onClick={() => value.moveCar('red', !value.redCar )}
+              type="button"
+            >
+              Move
+            </button>
+          </div>
+          <div>
+            <img
+              className={value.blueCar? 'car-right' : 'car-left'}
+              src={carBlue}
+              alt="blue car"
+            />
+            <button
+              onClick={() => value.moveCar('blue', !value.blueCar)}
+              type="button"
+            >
+              Move
+            </button>
+          </div>
+          <div>
+            <img
+              className={value.yellowCar ? 'car-right' : 'car-left'}
+              src={carYellow}
+              alt="yellow car"
+            />
+            <button
+              onClick={() => value.moveCar('yellow', !value.yellowCar)}
+              type="button"
+            >
+              Move
+            </button>
+          </div>
+        </div>
+      )}
+    </MyContext.Consumer>
   );
 }
 
