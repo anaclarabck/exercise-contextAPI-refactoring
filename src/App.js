c74b199323e8645ae8159e241f231067edb570be
anaclarabck <ackyotoku@gmail.com>
2021-07-21 19:34:39 -0300

    contextApi em TrafficSignal e Cars

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import TrafficSignal from './TrafficSignal';
 import './App.css';
 import Cars from './Cars';
+import CarsContext from './CarsContext'
 
-function App() {
-  return (
-    <div className="container">
-      <Cars />
-      <TrafficSignal />
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      redCar: false,
+      blueCar: false,
+      yellowCar: false,
+      signalColor: 'red'
+    }
+    this.moveCar = this.moveCar.bind(this);
+    this.changeSignal = this.changeSignal.bind(this);
+  }
+
+  moveCar(car, side) {
+    this.setState({
+      [car]: side,
+    })
+  }
+ 
+  changeSignal(color) {
+    this.setState({
+      signalColor: color,
+    })
+  }
+
+  render() {
+    const contextValue = {
+      ...this.state,
+      moveCar: this.moveCar,
+      changeSignal: this.changeSignal,
+    }
+
+    return (
+      <CarsContext.Provider value={ contextValue }>
+        <Cars />
+        <TrafficSignal />
+      </CarsContext.Provider>
+    )
+  }
 }
 
 export default App;
